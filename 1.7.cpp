47b56ef23b2c5823978767f6c51b30fd754ea620
cavemanjaw <cavemanjaw@op.pl>
2017-05-01 14:10:07 +0200

    Implementation of 1.7 started

---
+++ 1.7.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <stdlib.h>
+
+typedef std::vector<std::vector<int>> Array;
+
+void RandomFillArray(Array& array)
+{
+    for (auto& row: array)
+    {
+        for(auto& element: row)
+        {
+            element = rand();
+        }
+    }
+}
+
+void PrintArray(const Array& array)
+{
+for (auto row: array)
+    {
+        for (auto element: row)
+        {
+            std::cout << element;
+        }
+        std::cout << std::endl;
+    }
+}
+
+//Template this
+//The logic of filling with zeros could be inversed for optimisation
+void SetZeros(Array& array)
+{
+	std::vector<std::pair<int, int>> zerosCoordinates;
+	int rowSize = array.size();
+
+	//Assumption that those outer vectors are the same length
+	int columnSize = array.at(0).size();
+
+	for (int i = 0; i < rowSize; ++i)
+	{
+		for (int j = 0; j < columnSize; ++j)
+		{
+			if (array[i][j] == 0)
+			{
+				zerosCoordinates.push_back(std::make_pair(i, j));
+			}
+		}
+	}
+	
+	//Set rows to zeros
+	for (auto element: zerosCoordinates)
+	{
+		array.at(element.first) = std::vector<int>(columnSize, 0);
+	}
+
+	//Set columns to zero
+	for (auto element: zerosCoordinates)
+	{
+		//Iterate over rows
+		for (int i = 0; i < rowSize; ++i)
+		{
+			array.at(i).at(element.second) = 0;
+		}
+	}
+}
+
+int main()
+{
+    Array araj;
+    araj.resize(5);
+    for (int i = 0; i < 5; ++i)
+    {
+        araj.at(i).resize(5);
+    }
+    RandomFillArray(araj);
+    
+    PrintArray(araj);
+    
+    return 0;
+}
