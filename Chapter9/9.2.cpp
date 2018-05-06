7b6ceb11cb790a74818a0ec71ef4ec8d09fd62af
cavemanjaw_ubuntu <cavemanjaw@op.pl>
2018-05-06 20:11:33 +0200

    Further work on 9.2

--- Chapter9/9.2.cpp
+++ Chapter9/9.2.cpp
@@ -17,17 +17,42 @@ void SortByAnagrams(std::string array[], int stringArraySize)
    for (int i = 0; i < stringArraySize; ++i)
    {
       bool isCharInString[255] = {};
+      unsigned charCounter = 0;
    
       //Build up the isCharInString array
+      //If the iterator would be different type it can be used for counting
       for (auto k = array[i].begin(); k != array[i].end(); ++k)
       {
          isCharInString[(unsigned)(*k)] = true;
+         ++charCounter;
       }
 
       //Search for anagrams for i-th string in array
       for (int j = i + 1; j < stringArraySize; ++j)
       {
-         
+        //Clean this up, it can be done more efficiently
+        unsigned processedCharCounter = 0;
+
+        for (auto m = array[j].begin(); m != array[j].end(); ++m)
+        {
+           auto currentlyProcessedChar = *m;
+           
+           //Char is no in the array
+           if (isCharInString[(unsigned)(currentlyProcessedChar)] == false)
+           {
+              //Break the loop and do not do anything, this string is not an anagram of the processed one
+              break;
+           }
+           //If all characters have been processed it can be anagram, but if it is shorter there is also mismatch
+           ++processedCharCounter;
+        }
+        //If all characters have been processed it can be anagram, but if it is shorter there is also mismatch
+        if (charCounter == processedCharCounter)
+        {
+           //FOUND
+           //It is an anagram, act on this xD
+           //This functionality should be wrapped in some other function
+        }
       }
    }
 }
