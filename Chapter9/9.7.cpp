d4cc306dd4352040733a478cb2be111becdf61d6
cavemanjaw_ubuntu <cavemanjaw@op.pl>
2018-06-03 20:18:00 +0200

    Adding skeleton of 9.7

---
+++ Chapter9/9.7.cpp
@@ -0,0 +1,18 @@
+#include <utility>
+#include <vector>
+
+struct SortedMaxPossibleTower
+{
+   unsigned numberOfPeople;
+   std::vector<std::pair> sortedPeople;
+}
+
+SortedMaxPossibleTower ComputeMaxPossibleTower(std::vector<std::pair>& peopleInput)
+{
+   // Sort by first and then second member of pair?
+} 
+
+int main()
+{
+   return 0;
+}
